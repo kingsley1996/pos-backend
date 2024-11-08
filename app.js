84d906ba120a9b47c0f398a148f7c4cc3899fd06
samuel <linhdang.dev96@gmail.com>
2024-11-08 13:34:38 +0700

    config ngrok

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(cors({
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization', 'ngrok-skip-browser-warning']
 }));
 
 app.options('*', cors()); // Preflight request handler
